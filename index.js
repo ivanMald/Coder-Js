160a59caef8ea02dd0ce6c2b5d5bf4ae71404b3b
Ivan Maldonado <ivanvalentinmaldonado2014@hotmail.com>
2022-09-03 18:18:35 -0300

    funciones

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-let numeroIngresado = prompt ("Ingrese numero para cuenta atras")
+let newUsuario = prompt("Registrar usuario") 
+let newContraseña = prompt("Registrar contraseña")
+let usuario = prompt("Ingrese su usuario")
+let contraseña = prompt("Ingrese su contraseña")
 
 
-while(numeroIngresado >= 1){
-    document.write(numeroIngresado + " ");
-    numeroIngresado--;
-}
-if(numeroIngresado <= 1){
-    document.write("Termina la cuenta atras")
-}
-else if (numeroIngresado !== Number){
-    document.write("Numero invalido")
-}
 
+function iniciaSecion(){
+    if(usuario != newUsuario){
+        alert("Usuario incorrecto")
+    } 
+    if(contraseña != newContraseña){
+        alert("Contraseña incorrecta")
+    }else{
+        bienenida()
+    }
+}
+iniciaSecion()
+
+function bienenida(){
+    document.write("Bienvenido/a " + usuario)
+}
