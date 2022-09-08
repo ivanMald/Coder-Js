76cbec7077fb40fccd784e74d2828cca76e2697d
Ivan Maldonado <ivanvalentinmaldonado2014@hotmail.com>
2022-09-07 21:08:03 -0300

    comit arrays

--- index.js
+++ index.js
@@ -1,22 +1,43 @@
-let newUsuario = prompt("Registrar usuario") 
-let newContraseña = prompt("Registrar contraseña")
-let usuario = prompt("Ingrese su usuario")
-let contraseña = prompt("Ingrese su contraseña")
+// let newUsuario = prompt("Registrar usuario") 
+// let newContraseña = prompt("Registrar contraseña")
+// let usuario = prompt("Ingrese su usuario")
+// let contraseña = prompt("Ingrese su contraseña")
 
 
 
-function iniciaSecion(){
-    if(usuario != newUsuario){
-        alert("Usuario incorrecto")
-    } 
-    if(contraseña != newContraseña){
-        alert("Contraseña incorrecta")
-    }else{
-        bienenida()
-    }
+// function iniciaSecion(){
+//     if(usuario != newUsuario){
+//         alert("Usuario incorrecto")
+//     } 
+//     if(contraseña != newContraseña){
+//         alert("Contraseña incorrecta")
+//     }else{
+//         bienenida()
+//     }
+// }
+// iniciaSecion()
+
+// function bienenida(){
+//     document.write("Bienvenido/a " + usuario)
+// }
+const esconder = document.getElementById('esconder')
+
+const mostrar = document.getElementById('mostrar')
+
+
+const grupoEmojis =   ["🤡", "💩", "🎃", "🙀"]
+
+console.log(grupoEmojis)
+
+function esconderEmojis(){
+    document.write('Desaparesimos')
 }
-iniciaSecion()
 
-function bienenida(){
-    document.write("Bienvenido/a " + usuario)
-}
+function mostrarEmojis(){
+    document.write(grupoEmojis)
+} 
+
+
+
+mostrar.addEventListener('click', mostrarEmojis)
+esconder.addEventListener('click', esconderEmojis)
